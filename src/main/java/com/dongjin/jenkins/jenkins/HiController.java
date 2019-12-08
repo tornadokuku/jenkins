920b1220dd064d7ccc492ae4df83989737d24c54
yaohj <42960459@qq.com>
2019-12-08 08:34:16 +0800

    首次上传

---
+++ src/main/java/com/dongjin/jenkins/jenkins/HiController.java
@@ -0,0 +1,14 @@
+package com.dongjin.jenkins.jenkins;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+	
+	@RequestMapping(value = "/hello")
+    public String hi() {
+        return "hello yaohj ";  // 返回端口号，方便负载均衡功能测试
+    }
+}
---
+++ src/test/java/com/dongjin/jenkins/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongjin.jenkins.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

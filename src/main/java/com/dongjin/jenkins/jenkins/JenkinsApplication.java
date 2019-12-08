63f2f3342e76b9d983579c9c4ed9f78e260b744a
yaohj <42960459@qq.com>
2019-12-08 08:41:00 +0800

    首次提交

--- src/main/java/com/dongjin/jenkins/jenkins/JenkinsApplication.java
+++ src/main/java/com/dongjin/jenkins/jenkins/JenkinsApplication.java
@@ -1,7 +1,7 @@
 package com.dongjin.jenkins.jenkins;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsApplication {
